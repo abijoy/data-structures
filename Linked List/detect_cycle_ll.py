b479c21b13ce5ebb398e46bbc22452dd43a05663
Md Alamin <mdalaminbijoy3@gmail.com>
2023-03-07 01:31:23 +0600

    detect cycle in singly linked list

---
+++ Linked List/detect_cycle_ll.py
@@ -0,0 +1,43 @@
+from singly_ll import LinkedList, Node
+
+# Time --> O(n)  Space --> O(n)
+def detect_cycle_ll(head: Node) -> bool:
+    nodes = set()
+    curr_node = head
+    while curr_node:
+        print(curr_node.data)
+        if nodes.__contains__(curr_node):
+            return True
+        nodes.add(curr_node)
+        curr_node = curr_node.next
+    return False
+
+# Time --> O(n)  Space --> O(1)
+def detect_cycle_ll_runner_walker(head: Node) -> bool:
+    walker = head
+    runner = head 
+
+    while(runner.next and runner.next.next):
+        walker = walker.next
+        runner = runner.next.next
+        if (walker == runner):
+            return True
+    return False
+
+
+
+llist = LinkedList()
+llist.append(9)
+llist.append(8)
+llist.append(7)
+llist.append(6)
+
+# building a cycle
+llist.head.next.next.next.next = llist.head
+
+
+# llist.printList()
+
+print(detect_cycle_ll(llist.head))
+print(detect_cycle_ll_runner_walker(llist.head))
+
